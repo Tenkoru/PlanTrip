a5a1abcad67fc12ba91fe93a9f8e530164bb13f4
Александр Стрелец <astrelets@inbox.ru>
2019-08-24 13:35:46 +0300

    add password vision switcher

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
 "use strict";
 
 const inputs = [].slice.call(document.querySelectorAll('.input'));
+const passwordSwitchers = [].slice.call(document.querySelectorAll('.auth__password-switcher'));
 
 function inputPlaceholderHandler() {
     this.value ? this.classList.add('input--filled') : this.classList.remove('input--filled');
 };
+function passwordViewSwitch() {
+    const passwordInput = this.parentNode.querySelector('.auth__input--password');
+
+    if (this.classList.contains('auth__password-switcher--active')) {
+        passwordInput.type = 'password';
+        this.classList.remove('auth__password-switcher--active');
+    } else {
+        passwordInput.type = 'text';
+        this.classList.add('auth__password-switcher--active');
+    }
+}
 
 inputs.forEach(function(item) {
     item.addEventListener('input', inputPlaceholderHandler);
+});
+passwordSwitchers.forEach(function(item) {
+    item.addEventListener('click', passwordViewSwitch);
 });
