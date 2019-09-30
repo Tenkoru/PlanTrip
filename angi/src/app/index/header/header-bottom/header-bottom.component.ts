0c23189c602d144f59aab49ba222e116d9349b29
Александр Стрелец <astrelets@inbox.ru>
2019-10-01 02:27:06 +0300

    add back button

--- angi/src/app/index/header/header-bottom/header-bottom.component.ts
+++ angi/src/app/index/header/header-bottom/header-bottom.component.ts
@@ -1,17 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterState, Router, NavigationEnd } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-header-bottom',
-  templateUrl: './header-bottom.component.html',
-  styleUrls: ['./header-bottom.component.scss']
+  selector: "app-header-bottom",
+  templateUrl: "./header-bottom.component.html",
+  styleUrls: ["./header-bottom.component.scss"]
 })
 export class HeaderBottomComponent implements OnInit {
-
   title: string = "Список поездок";
+  titles = {
+    dashboard: "Список поездок",
+    details: "Информация о поездке",
+    friends: "Друзья",
+  }
+  isArrowShown = true;
+  arrowProps = {
+    link: "/dashboard",
+    className: "headerArrow",
+  }
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.setTitleByUrl(event.urlAfterRedirects);
+      }
+    });
+  }
+  setTitleByUrl(url: string) {
+    this.isArrowShown = true;
+    if (url.match("dashboard")) {
+      this.isArrowShown = false;
+      this.title = this.titles.dashboard;
+    } else if (url.match("details")) {
+      this.title = this.titles.details;
+    } else if (url.match("friends")) {
+      this.title = this.titles.friends;
+    }
+  }
 
   ngOnInit() {
   }
-
 }
